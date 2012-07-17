976dc205992399b4abd21ccb07c115fb61c9d8e9
Aaron Hicks (Landcare PN) <hicksa@landcareresearch.co.nz>
2012-07-17 13:28:07 +1200

    trying to access a resource's values

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -48,10 +48,10 @@ class postgresql::server::install(
   file {"pg_hba.conf":
     path    => "/etc/postgresql/${version}/main/pg_hba.conf",
     source  => [
-      "puppet:///modules/database/postgresql/${version}/${fqdn}/pg_hba.conf",
-      "puppet:///modules/database/postgresql/${version}/${operatingsystem}/pg_hba.conf",
-      "puppet:///modules/database/postgresql/${version}/pg_hba.conf",
-      "puppet:///modules/database/postgresql/pg_hba.conf",
+      "puppet:///modules/postgresql/${version}/${fqdn}/pg_hba.conf",
+      "puppet:///modules/postgresql/${version}/${operatingsystem}/pg_hba.conf",
+      "puppet:///modules/postgresql/${version}/pg_hba.conf",
+      "puppet:///modules/postgresql/pg_hba.conf",
     ],
     mode    => 640,
     require => [Package['postgresql'],User['postgres']],
@@ -62,10 +62,13 @@ class postgresql::server::install(
 
   file {"postgresql.conf":
     path    => "/etc/postgresql/${version}/main/postgresql.conf",
-    content => template("database/postgresql/${version}/postgresql.conf.erb"),
+    content => template("postgresql/${version}/postgresql.conf.erb"),
     require => Package['postgresql'],
     notify  => Service['postgresql'],
   }
 
-}
+  $foo = File['postgresql.conf']['path']
+
+  warning("Does this work: ${foo}")
+}  
 
