d0596cd383aaddf2cbbabfc0c796758adc438d95
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-06-14 12:29:40 +1200

    change tabs to spaces

--- manifests/pg_hba.pp
+++ manifests/pg_hba.pp
@@ -4,43 +4,43 @@
 # If you need it later, code it later.
 
 define postgresql::pg_hba(
-	$ensure				= present,
-	$type					= 'local',
-	$databases		= ['all'],
-	$user 				= 'all',
-	$host					= false,
-	$auth_method	= 'md5'
+  $ensure       = present,
+  $type         = 'local',
+  $databases    = ['all'],
+  $user         = 'all',
+  $host         = false,
+  $auth_method  = 'md5'
 ){
 
-	if ! ($auth_method in ['md5','trust','reject','peer','password']) {
-		$error_msg = "The authorisation METHOD '${auth_method}' is not supported in postgresql::pg_hba on ${fqdn}."
-	}
+  if ! ($auth_method in ['md5','trust','reject','peer','password']) {
+    $error_msg = "The authorisation METHOD '${auth_method}' is not supported in postgresql::pg_hba on ${fqdn}."
+  }
 
-	$db_string = inline_template("<%= databases.join(',') %>")
+  $db_string = inline_template("<%= databases.join(',') %>")
 
-	if ! $error_msg {
-		case $type {
-			local:{
-				$pg_hba_line = "${type} 	${db_string} 	${user} 			${auth_method}"
-			}
-			host:{
-				$pg_hba_line = "${type} 	${db_string} 	${user} 	${host} 	${auth_method}"
-			}
-			default:{
-				$error_msg = "The TYPE '${type}' is not supported in postgresql::pg_hba on ${fqdn}"
-			}
-		}
-	}
+  if ! $error_msg {
+    case $type {
+      local:{
+        $pg_hba_line = "${type}   ${db_string}  ${user}       ${auth_method}"
+      }
+      host:{
+        $pg_hba_line = "${type}   ${db_string}  ${user}   ${host}   ${auth_method}"
+      }
+      default:{
+        $error_msg = "The TYPE '${type}' is not supported in postgresql::pg_hba on ${fqdn}"
+      }
+    }
+  }
 
-	if ! $error_msg {
-		file_line{'${type}_${user}_${auth_method}_pghba':
-			ensure		=> $ensure,
-			line 			=> $pg_hba_line,
-			path 			=> "/etc/postgresql/${postgresql::server::version}/main/pg_hba.conf",
-			notify		=> Service['postgresql'],
-		}
-	} else {
-		err($error_msg)
-	}
+  if ! $error_msg {
+    file_line{'${type}_${user}_${auth_method}_pghba':
+      ensure    => $ensure,
+      line      => $pg_hba_line,
+      path      => "/etc/postgresql/${postgresql::server::version}/main/pg_hba.conf",
+      notify    => Service['postgresql'],
+    }
+  } else {
+    err($error_msg)
+  }
 
 }
