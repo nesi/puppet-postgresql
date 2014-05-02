cd07ec0964d0b585c028bf249c10260fe55d5184
Cevaris <cevaris@gmail.com>
2014-05-01 23:08:13 -0600

    added postgres client packages postgresql-devel and libpq-dev

--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -2,6 +2,13 @@ class postgresql::client::install(
   $version
 ){
   
+  package{'libpq-dev':
+    ensure  => installed,
+    name    => $operatingsystem ? {
+      Ubuntu  => "libpq-dev",
+      default => 'postgresql-devel',
+    }
+  }
   package{'postgresql_client':
     ensure  => installed,
     name    => $operatingsystem ? {
